251814d932e8718b910c6d9cf08fe7e9bbbd7bd1
민재 유 <111565097+arpa0091@users.noreply.github.com>
2023-12-31 14:35:25 +0900

    코이바보

--- key.py
+++ key.py
@@ -1,16 +1,18 @@
 # Discord Bot Token
-discord_key = "Your_Bot_Token"
+discord_key = "MTE5MDYzMjA2ODE2MjUzMTMyOA.GGcwZC.YL1CmSqp1HWIEkZKVQRhKAJSrnYDwIjOieXbyg"
 
 # Neis API Token and URL(Change SC_CODE ansd SCHUL_CODE that match your school)
+#Will disapper 
 SC_CODE = "Your_Code"
 SD_SCHUL_CODE = "Your_Code"
-meal_URL = "https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=" + SC_CODE + "&SD_SCHUL_CODE=" + SD_SCHUL_CODE + "&type=json&MLSV_YMD="
+meal_URL = f"https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE={SC_CODE}&SD_SCHUL_CODE={SD_SCHUL_CODE}&type=json&MLSV_YMD="
 meal_key = "&KEY=Your_Key"
+# USE f-string
 
 # SCS Server's ID
-Slash_Command_Server = [Your_Guild_Id]
+Slash_Command_Server = ['1030403377152131082']
 
 # Debug config
 discord_key_debug = "YOUR_DEBUG_BOT_TOKEN"
 meal_key_debug = "&KEY=YOUR_DEBUG_KEY"
-Slash_Command_Server_debug = [YOUR_DEBUG_GUILD_ID]
+Slash_Command_Server_debug = ['1030403377152131082']
